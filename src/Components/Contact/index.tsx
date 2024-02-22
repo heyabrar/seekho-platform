463bdfcf3289db786aabb23d0c4b222b65227d99
Abrar Khan <heyabrarkhan@gmail.com>
2024-02-22 15:43:59 +0530

    refactor: Email and whats app linked

--- src/Components/Contact/index.tsx
+++ src/Components/Contact/index.tsx
@@ -2,16 +2,31 @@ import "../../styles/contact.css";
 import WhatsApp from "../Icons/WhatsApp";
 
 const Contact = () => {
+  const mailAddress = "queries@seekhoapp.com";
+  const phoneNumber = "7905985350";
+
+  const handleWhatsAppClick = () => {
+    const whatsappUrl = `whatsapp://send?phone=${phoneNumber}`;
+    window.location.href = whatsappUrl;
+  };
+
   return (
     <div className="contact-container">
       <p>Aapko koi sawaal hai?</p>
       <textarea placeholder="Humse poochen" className="text-area" />
-      <button className="send-cta">
+      <button className="send-cta" onClick={handleWhatsAppClick}>
         <WhatsApp /> Bhejein
       </button>
       <p className="email">
         Ya fir humein email karein{" "}
-        <span style={{ color: " #ffcc11" }}>queries@seekhoapp.com</span>
+        <a
+          href={`mailTo:${mailAddress}`}
+          style={{ color: " #ffcc11" }}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {mailAddress}
+        </a>
       </p>
     </div>
   );
