a780cf264af81269328cef790b97a8d05417fdb8
Abrar Khan <heyabrarkhan@gmail.com>
2024-02-22 13:27:45 +0530

    add: Faq indentation

--- src/Components/FAQ/FaqItem.tsx
+++ src/Components/FAQ/FaqItem.tsx
@@ -2,18 +2,21 @@ import { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 
 type Props = {
-    question: string;
-    answer: string;
+  question: string;
+  answer: string;
 };
 
 const FaqItem = ({ question, answer }: Props) => {
-    const [clicked, setClicked] = useState(false)
-    return (
-        <div className="faq-item" onClick={() => setClicked(!clicked)}>
-            <p style={{ display: 'flex', justifyContent: 'space-between' }}>{question} <FaChevronDown className="chevron-down"/></p>
-            {clicked && <p>{answer}</p>}
-        </div>
-    );
+  const [clicked, setClicked] = useState(false);
+  
+  return (
+    <div className="faq-item" onClick={() => setClicked(!clicked)}>
+      <p style={{ display: "flex", justifyContent: "space-between" }}>
+        {question} <FaChevronDown className="chevron-down" />
+      </p>
+      {clicked && <p>{answer}</p>}
+    </div>
+  );
 };
 
 export default FaqItem;
--- src/Components/FAQ/index.tsx
+++ src/Components/FAQ/index.tsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import useStore from "../../store";
 import FaqItem from "./FaqItem";
 
 const Faq = () => {
-    const { faq } = useStore();
+  const { faq } = useStore();
 
-    console.log({ faq });
-    return (
-        <div className="faq-container">
-            <p>{faq?.title}</p>
-
-            <div>
-                {faq?.faq_list?.map((item) => {
-                    return (
-                        <FaqItem question={item.question} answer={item.answer} />
-                    );
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div className="faq-container">
+      <p>{faq?.title}</p>
+      <div>
+        {faq?.faq_list?.map((item) => {
+          return <FaqItem question={item.question} answer={item.answer} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
